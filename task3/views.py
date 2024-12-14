a186b224f51792e0fc50739d26aab62cf086b0e2
Roman_Tretiakov <romantre@wizardsdev.com>
2024-12-14 17:47:12 +0200

    commit task3 updated

--- task3/views.py
+++ task3/views.py
@@ -1,15 +1,30 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
-# Create your views here.
-class Main(TemplateView):
+class BaseView(TemplateView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['main_page'] = 'Главная страница'
+        context['main'] = 'Главная'
+        context['shop'] = 'Магазин'
+        context['basket'] = 'Корзина'
+        context['games'] = 'Игры'
+        context['game_1'] = 'Atomic Heart'
+        context['game_2'] = 'Cyberpunk 2077'
+        context['game_3'] = 'PayDay 2'
+        context['buy_button'] = 'Купить'
+        context['back_button'] = 'Вернуться обратно'
+        context['basket_message'] = 'Извините, ваша корзина пуста!'
+        return context
+
+
+class Main(BaseView):
     template_name = 'third_task/main_page_template.html'
 
 
-class Shop(TemplateView):
+class Shop(BaseView):
     template_name = 'third_task/shop_template.html'
 
 
-class Basket(TemplateView):
+class Basket(BaseView):
     template_name = 'third_task/basket_template.html'
