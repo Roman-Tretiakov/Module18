3360a4adf7e20415589e565254998b54104a7e0a
Roman_Tretiakov <romantre@wizardsdev.com>
2024-12-15 21:17:08 +0200

    commit task5

---
+++ task5/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+users = ['John', ]
+
+def sign_up_by_html(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        repeat_password = request.POST['repeat_password']
+        age = request.POST['age']
+        return HttpResponse("")
+
+    return render(request, 'registration_page.html')
